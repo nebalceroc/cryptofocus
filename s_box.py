8fb6e438090573a5a2fbba9a88572eeded5c4945
jecalderonc <jecalderonc@unal.edu.co>
2014-09-12 15:17:02 -0500

    clase DES Signed-off-by: jecalderonc <jecalderonc@unal.edu.co>

--- s_box.py
+++ s_box.py
@@ -6,9 +6,12 @@ class S_Box():
     def cypherSegment(self, segment):              
         entrada=int(segment,2)
         salida=self.key[entrada]-1
+
         salida=bin(salida)[2:]
+
         if (len(salida) % 3 != 0):
             for x in range(3-len(salida)%3):
                 salida='0'+salida
+
                 
         return salida
